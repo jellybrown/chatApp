a4239d40bcd4e09b42b3af584c922a2541e07b85
yujin mac <kimyu28jin@gmail.com>
2020-12-08 16:33:13 +0900

    sidePanel의 directMessage부분 ui추가

--- src/components/ChatPage/SidePanel/directMessage.jsx
+++ src/components/ChatPage/SidePanel/directMessage.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import {HiOutlineChevronDown} from 'react-icons/hi';
 import './sideStyles.css';
+
+
+
 class DirectMessage extends Component {
+    renderDirectMessages = () => {
+
+    }
+
     render() {
         return (
-            <div>
-                DirectMessage
+            
+            <div className="dmChat">
+                    <div className="title">
+                        <HiOutlineChevronDown size="1.2rem"/>
+                        <span>Direct Messages (1) </span>
+                    </div>
+                
+                <ul className="dmList">
+                    {this.renderDirectMessages()}
+                </ul>
+
             </div>
         );
     }
